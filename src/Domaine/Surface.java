e2a58fecb676dd2ef7827ba9541e9f65a7705b16
Catherine Simard <catherine.simard.30@ulaval.ca>
2019-10-30 11:55:52 -0400

    Debut implementation Surface (méthodes)

--- src/Domaine/Surface.java
+++ src/Domaine/Surface.java
@@ -1,10 +1,43 @@
 package Domaine;
+
 import java.awt.Point;
 import java.util.ArrayList;
+import java.awt.Polygon;
 
 public class Surface {
-    public Surface(ArrayList<Point> listPoints){
 
+    public Polygon polygone;
+
+    public Surface(ArrayList<Point> listPoints) {
+    }
+    //méthode contains de Polygon
+    //return True si le point est dans la surface
+    public boolean estDansSurface(Point point){
+        return false;
+    }
+    //méthode permettant de déplacer une surface en se basant sur le point en haut à gauche
+    public void deplacerSurface(Point point){
+
+    }
+    public void modifierSommets(){
+
+    }
+    public void modifierSurface(){
+
+    }
+    public void modifierTypeSurface(){
+
+    }
+    public void getInfoSurface(){
+
+    }
+    public void editerAlligmentSurface(){
+
+    }
+    public void deplacerModifierMotif(){
+
+    }
+    public void editerMotif(){
 
     }
 }
